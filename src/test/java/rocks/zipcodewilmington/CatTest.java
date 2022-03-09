6179cec6fd34aa53e6699d0f29909e0a446e8d78
Clyde Broderick <clyderick3@gmail.com>
2022-03-08 23:48:10 -0500

    Passed CatTestsetName.

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -11,6 +11,18 @@ import java.util.Date;
  */
 public class CatTest {
     // TODO - Create tests for `void setName(String name)`
+    @Test //@Test is how you annotate a test.
+    public void CatTestsetName() {
+        //Given
+        Cat testCat = new Cat("Joe", new Date(), 1); //testCat is the object.
+        String expected = "Kitty"; //Started his name as Joe and set it to Kitty.
+
+        //When
+        testCat.setName(expected);
+        //Then
+        String actual = testCat.getName(); //getName exists purely to return a value.
+        Assert.assertEquals(expected, actual); //Expected is what it supposed to be. Actual is what it's coming back as.
+    }
     // TODO - Create tests for `speak`
     // TODO - Create tests for `setBirthDate(Date birthDate)`
     // TODO - Create tests for `void eat(Food food)`
